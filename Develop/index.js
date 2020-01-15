358d6cc85da5818be41623bcdf95098b5eb268a9
IamPopov <ivantsar@gmail.com>
2020-01-14 21:44:53 -0700

    installed npm packages and started writing code

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,43 @@
+const axios = require("axios");
+const inquirer = require("inquirer");
+const pdf = require('html-pdf');
+
+const fs = require('fs');
+const html = fs.readFileSync('index.html', 'utf8'); // what is this?
+
+inquirer
+.prompt([{
+    message: "Enter your Github username",
+    name: "username"
+},
+{
+    type: "list",
+    message: "what is your favorite color?",
+    name: "color",
+    choices: ['green', 'blue', 'pink', 'red']
+}])
+
+.then(function({username}) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    axios 
+      .get(queryUrl)
+      .then(function(res) {
+        let gitArray = res.data;
+        
+        pdf.create(html, options).toFile('')
+
+      }) 
+
+})
+
+// fs.writeFile("repo.txt", gitArray.map(function(item) {
+//     return item.name
+//   }), err => {
+//     if (err) throw err;
+//     return console.log('saved');
+//   }
+//   )
+
 const questions = [
   
 ];
@@ -8,4 +48,4 @@ function writeToFile(fileName, data) {
 
 function init() {
 
-init();
+//init();
